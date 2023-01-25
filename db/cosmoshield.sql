c69ce069dd6db6594066d64493b71d5cc2feb07a
Yogesh Saroya <yogeshsaroya@gmail.com>
2023-01-25 10:46:40 +0530

    db

--- db/cosmoshield.sql
+++ db/cosmoshield.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 23, 2023 at 06:12 AM
+-- Generation Time: Jan 25, 2023 at 03:49 AM
 -- Server version: 5.7.39
--- PHP Version: 7.4.33
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -48,7 +48,8 @@ INSERT INTO `messages` (`id`, `user_id`, `message`, `message_type`, `created`, `
 (5, 9, 'Hello', 1, '2023-01-22 07:24:23', '2023-01-22 07:24:23'),
 (6, 9, 'How can i help you?', 2, '2023-01-22 10:18:47', '2023-01-22 10:18:47'),
 (7, 10, 'Hi', 1, '2023-01-22 10:37:17', '2023-01-22 10:37:17'),
-(8, 10, 'How can i help you?', 2, '2023-01-22 10:38:01', '2023-01-22 10:38:01');
+(8, 10, 'How can i help you?', 2, '2023-01-22 10:38:01', '2023-01-22 10:38:01'),
+(9, 10, 'asdfdsf', 2, '2023-01-24 05:03:36', '2023-01-24 05:03:36');
 
 -- --------------------------------------------------------
 
@@ -99,7 +100,7 @@ CREATE TABLE `reports` (
 --
 
 INSERT INTO `reports` (`id`, `domain`, `reason`, `email`, `status`, `whois_data`, `whois_data_date`, `created`, `modified`) VALUES
-(1, 'sadf', 'sadf', 'saroya.com@gmail.com', 1, NULL, NULL, '2023-01-20 07:29:06', '2023-01-20 07:29:06'),
+(1, 'sadf', 'sadf', 'saroya.com@gmail.com', 3, NULL, NULL, '2023-01-20 07:29:06', '2023-01-24 05:03:43'),
 (2, 'abc.com', 'sadfsd', 'saroya.com@gmail.com', 1, NULL, NULL, '2023-01-20 07:40:36', '2023-01-22 08:10:26'),
 (3, 'abc.com	', 'sdff', 'saroya.com@gmail.com', 3, NULL, NULL, '2023-01-20 07:40:54', '2023-01-22 08:10:33'),
 (4, 'abasdf.in', 'abc.com	', 'saroya.com@gmail.com', 1, NULL, NULL, '2023-01-20 07:50:56', '2023-01-22 08:10:21'),
@@ -120,7 +121,7 @@ CREATE TABLE `settings` (
   `email_password` varchar(100) DEFAULT NULL,
   `email_host` varchar(100) DEFAULT NULL,
   `email_port` int(11) DEFAULT NULL,
-  `whois_api_key` varchar(64) DEFAULT NULL COMMENT 'https://www.ip2location.io/dashboard',
+  `whois_api_key` varchar(64) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -130,7 +131,7 @@ CREATE TABLE `settings` (
 --
 
 INSERT INTO `settings` (`id`, `email_sender`, `email_address`, `email_password`, `email_host`, `email_port`, `whois_api_key`, `created`, `modified`) VALUES
-(1, 'support', 'support@superpad.finance', 'super@1234!', 'mail.superpad.finance', 587, '15BD37B169896A518A3D2DA6348D1640', '2022-03-15 13:20:49', '2023-01-23 00:00:19');
+(1, 'support', 'support@superpad.finance', 'super@1234!', 'mail.superpad.finance', 587, 'at_ix50p08KlWE3m8qNqHiltafU1aFvh', '2022-03-15 13:20:49', '2023-01-23 00:00:19');
 
 -- --------------------------------------------------------
 
@@ -188,7 +189,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `username`, `password`, `role`, `reset_password_key`, `reset_password_key_expiry`, `created`, `modified`) VALUES
-(9, NULL, NULL, 'saroya.com@gmail.com', 'admin@admin.com', '$2y$10$5Y31Kwu89Fs7cBeeqvV/suPBk3bCAc7we3Cim.RRL8c2rdxLWDgDK', 1, NULL, NULL, '2023-01-19 07:10:55', '2023-01-20 05:39:45'),
+(9, NULL, NULL, 'admin@admin.com', 'admin@admin.com', '$2y$10$5Y31Kwu89Fs7cBeeqvV/suPBk3bCAc7we3Cim.RRL8c2rdxLWDgDK', 1, NULL, NULL, '2023-01-19 07:10:55', '2023-01-20 05:39:45'),
 (10, NULL, NULL, 'cakephp.com@gmail.com', 'cakephp.com', '$2y$10$lE52zjWLRXNqm2/yvbAly.3lZ4hNsnqr4EqUkkp4VOTTPEG0SUm3.', 2, NULL, NULL, '2023-01-22 10:31:18', '2023-01-22 10:36:44');
 
 -- --------------------------------------------------------
@@ -276,7 +277,7 @@ ALTER TABLE `wallets`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `photos`
