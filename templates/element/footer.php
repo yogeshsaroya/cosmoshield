2ce156c11d9995aa1eb155b5007f385f92d5eadc
Yogesh Saroya <yogeshsaroya@gmail.com>
2023-01-25 16:30:15 +0530

    v2.1

--- templates/element/footer.php
+++ templates/element/footer.php
@@ -1,3 +1,3 @@
 <footer class="text-center">
-   <p class="mb-0">© <?= date('Y') - 1;?> - <?= date('Y');?> Interbloc LLC, All rights reserved.</p>
+   <p class="mb-0">© <?= date('Y') - 1;?> - <?= date('Y');?> Cosmo Recovery LLC, All rights reserved.</p>
 </footer>
